65601a810f26da3adac9952d8b692ae6f6dbe83d
user.name like: "oleg@kishinskiy.com <oleg@kishinskiy.com>
2019-04-03 13:39:57 +0300

    Add Array

---
+++ L2.1-ArrayList/src/com/kishinskiy/Main.java
@@ -0,0 +1,13 @@
+package com.kishinskiy;
+
+public class Main {
+
+    public static void main (String []args){
+        MyArrayList myArrayList = null;
+        myArrayList.add("test1");
+    }
+
+
+
+
+}
