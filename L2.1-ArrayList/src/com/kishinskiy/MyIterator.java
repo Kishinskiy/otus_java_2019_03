656d7d8144802535d95650fd120b477d02918eed
user.name like: "oleg@kishinskiy.com <oleg@kishinskiy.com>
2019-04-20 22:03:42 +0300

    add new Class

---
+++ L2.1-ArrayList/src/com/kishinskiy/MyIterator.java
@@ -0,0 +1,37 @@
+package com.kishinskiy;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLDocument;
+
+public class MyIterator  extends HTMLDocument.Iterator {
+    @Override
+    public AttributeSet getAttributes() {
+        return null;
+    }
+
+    @Override
+    public int getStartOffset() {
+        return 0;
+    }
+
+    @Override
+    public int getEndOffset() {
+        return 0;
+    }
+
+    @Override
+    public void next() {
+
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public HTML.Tag getTag() {
+        return null;
+    }
+}
