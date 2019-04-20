02602fd5ecc6c3daf67406782391a0d1198ec94c
user.name like: "oleg@kishinskiy.com <oleg@kishinskiy.com>
2019-04-20 21:58:46 +0300

    Add Methods and pom files

--- L2.1-ArrayList/src/com/kishinskiy/Main.java
+++ L2.1-ArrayList/src/com/kishinskiy/Main.java
@@ -4,86 +4,71 @@ import java.util.*;
 
 public class Main {
 
-//    // create array list object
-//    List arrlist = new ArrayList();
-//
-//    // populate the list
-//      arrlist.add("A");
-//      arrlist.add("B");
-//      arrlist.add("C");
-//
-//      System.out.println("Initial collection value: "+arrlist);
-//
-//    // add values to this collection
-//    boolean b = Collections.addAll(arrlist, "1","2","3");
-//
-//      System.out.println("Final collection value: "+arrlist);
-//}
-
-
-//    // create two lists
-//    List<String> srclst = new ArrayList<String>(5);
-//    List<String> destlst = new ArrayList<String>(10);
-//
-//    // populate two lists
-//      srclst.add("Java");
-//      srclst.add("is");
-//      srclst.add("best");
-//
-//      destlst.add("C++");
-//      destlst.add("is");
-//      destlst.add("older");
-//
-//    // copy into dest list
-//      Collections.copy(destlst, srclst);
-//
-//      System.out.println("Value of source list: "+srclst);
-//      System.out.println("Value of destination list: "+destlst);
-
-
     public static void main(String[] args) {
 
+        //////  вызываем addAll /////
+
         List arrlist = new MyArray<>();
+
 
         arrlist.add("A");
         arrlist.add("B");
         arrlist.add("C");
 
 
-
-            System.out.println("String Values: " + arrlist);
+        System.out.println("String Values: " + arrlist);
 
         System.out.println("String Values: " + arrlist);
 
-        boolean b = Collections.addAll(arrlist, "1", "2", "3");
+        boolean b = Collections.addAll(arrlist, "1", "2", "3"); // с этим вроде бы все понятно.
 
         System.out.println("Complete Values: " + arrlist);
 
+        ////////////////////////////////////////
 
-            // create two lists
-    List<String> srclst = new MyArray<String>(5);
-    List<String> destlst = new MyArray<String>(10);
 
-    // populate two lists
-      srclst.add("Java");
-      srclst.add("is");
-      srclst.add("best");
+        ///////   copy ////////
 
-      destlst.add("C++");
-      destlst.add("is");
-      destlst.add("older");
+        // create two lists
+        try {
+            List<String> srclst = new MyArray<String>(5);
+            List<String> destlst = new MyArray<String>(10);
 
-    // copy into dest list
-      Collections.copy(destlst, srclst);
+            srclst.add("Java");
+            srclst.add("is");
+            srclst.add("best");
 
-      System.out.println("Value of source list: "+srclst);
-      System.out.println("Value of destination list: "+destlst);
+            destlst.add("C++");
+            destlst.add("is");
+            destlst.add("older");
 
+            Collections.copy(destlst, srclst);  // java.lang.NullPointerException  почему, не понимаю
+
+            System.out.println("Value of source list: " + srclst);
+            System.out.println("Value of destination list: " + destlst);
+
+
+        } catch (NullPointerException e) {
+            System.out.println("Ошибка при копировании эллементов массива");
+            //////////////////////////////////////
+
+
+            //// Sort /////
+            MyArray<Integer> list = new MyArray<Integer>();
+            list.add(-28);
+            list.add(20);
+            list.add(-12);
+            list.add(8);
+
+            Collections.sort(list, null);  // с этим тоже все ясно
+
+            System.out.println("List sorted in natural order: ");
+
+            for (int i : list) {
+                System.out.println(i + " ");
+            }
+            //////////////////////////////////
+        }
 
     }
-
-
-
-
-
 }
