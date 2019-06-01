40c216d02a982213feda17167fbfae9774548548
user.name like: "oleg@kishinskiy.com <oleg@kishinskiy.com>
2019-06-01 15:44:23 +0300

    Add tests class

--- LO6-Anatation/src/com/kishinskiy/Main.java
+++ LO6-Anatation/src/com/kishinskiy/Main.java
@@ -2,21 +2,7 @@ package com.kishinskiy;
 
 public class Main {
 
-    @Before
-    public void before(){
-        System.out.println("Before Anatation...");
-    }
 
-    @Test
-    public void test (){
-
-        System.out.println("Test Anatation...");
-    }
-
-    @After
-    public void after(){
-        System.out.printf("After Anatation...");
-    }
 
     public static void main(String[] args){
 
---
+++ LO6-Anatation/src/com/kishinskiy/Tests.java
@@ -0,0 +1,15 @@
+@Before
+public void before(){
+        System.out.println("Before Anatation...");
+        }
+
+@Test
+public void test (){
+
+        System.out.println("Test Anatation...");
+        }
+
+@After
+public void after(){
+        System.out.printf("After Anatation...");
+        }
